5517e4903f88a8523e9bd241a3d55c3c4cb6a06a
Roy Liu <carsomyr@gmail.com>
2012-06-13 16:00:08 -0400

    Add the scalient cookbook

---
+++ vendor/cookbooks/scalient/metadata.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2012 Scalient LLC
+
+require "pathname"
+
+maintainer "Roy Liu"
+maintainer_email "roy@scalient.net"
+license "All rights reserved"
+description "The Chef cookbook for Scalient LLC"
+long_description Pathname.new("../README.md").expand_path(__FILE__).open { |f| f.read }
+version "0.9.0"

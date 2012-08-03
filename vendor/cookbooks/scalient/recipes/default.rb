194a38542ea52ef447382c9f3b5454e3229f8fc0
Roy Liu <carsomyr@gmail.com>
2012-08-03 03:46:16 -0400

    scalient: Add the template["/etc/hosts"] and template["/etc/hostname"] resources

--- vendor/cookbooks/scalient/recipes/default.rb
+++ vendor/cookbooks/scalient/recipes/default.rb
@@ -5,8 +5,43 @@
 require "pathname"
 
 gemfile = Pathname.new("client/Gemfile.d/Gemfile-scalient").expand_path(Dir.home("chef"))
+has_rabbitmq_server = Pathname.new("/usr/sbin/rabbitmqctl").executable?
 
 cap_ops_gemfile_fragment gemfile.to_s do
   source gemfile.basename.to_s
   action :nothing
 end.action(:create)
+
+cap_ops_recreate_rabbit_queue "reload-hostname" do
+  action :nothing
+end
+
+template "/etc/hosts" do
+  source "hosts.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(:fqdn => node.name,
+            :hostname => node.name.split(".", -1)[0])
+  action :nothing
+end.action(:create)
+
+template "/etc/hostname" do
+  source "hostname.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(:hostname => node.name.split(".", -1)[0])
+  notifies :run, "cap_ops_recreate_rabbit_queue[reload-hostname]", :immediately if has_rabbitmq_server
+  notifies :run, "bash[hostname]", :immediately if !has_rabbitmq_server
+  action :nothing
+end.action(:create)
+
+bash "hostname" do
+  user "root"
+  group "root"
+  code <<EOF
+exec -- hostname -F /etc/hostname
+EOF
+  action :nothing
+end

84880d1b75a26fe7085766d28c6df90cac194259
Roy Liu <carsomyr@gmail.com>
2012-09-20 15:54:39 -0400

    scalient: Add YAML templates for propagating data bag settings to Rails applications

--- vendor/cookbooks/scalient/recipes/rails.rb
+++ vendor/cookbooks/scalient/recipes/rails.rb
@@ -9,6 +9,8 @@ end
 require "pathname"
 
 recipe = self
+user_home = Dir.home(recipe.original_user)
+app_dir = Pathname.new("apps").join(node.name.split(".", -1)[1]).expand_path(user_home)
 
 apt_repository "passenger-nginx" do
   uri "http://ppa.launchpad.net/brightbox/passenger-nginx/ubuntu"
@@ -69,7 +71,7 @@ template "/etc/nginx/sites-available/default" do
   owner "root"
   group "root"
   mode 0644
-  variables(:app_root => Pathname.new("apps/scalient/current/public").expand_path(Dir.home(recipe.original_user)).to_s,
+  variables(:app_root => app_dir.join("current", "public").to_s,
             :passenger_ruby => recipe.ruby_interpreter_path)
   notifies :restart, "service[nginx]", :immediately
   action :nothing
@@ -78,3 +80,47 @@ end.action(:create)
 service "nginx" do
   action :nothing
 end
+
+[app_dir.parent,
+ app_dir,
+ app_dir.join("releases"),
+ app_dir.join("shared"),
+ app_dir.join("shared", "config"),
+ app_dir.join("shared", "log"),
+ app_dir.join("shared", "pids"),
+ app_dir.join("shared", "system")].each do |dir|
+  directory dir.to_s do
+    owner recipe.original_user
+    group recipe.original_group
+    mode 0755
+    action :nothing
+  end.action(:create)
+end
+
+template app_dir.join("shared", "config", "airbrake.yml").to_s do
+  source "airbrake.yml.erb"
+  owner recipe.original_user
+  group recipe.original_group
+  mode 0644
+  variables(:api_key => data_bag_item("monitoring", "airbrake")[node.name])
+  action :nothing
+end.action(:create)
+
+template app_dir.join("shared", "config", "aws.yml").to_s do
+  source "aws.yml.erb"
+  owner recipe.original_user
+  group recipe.original_group
+  mode 0644
+  variables(:access_key => data_bag_item("keys", "aws")["access_key"],
+            :secret_key => data_bag_item("keys", "aws")["secret_key"])
+  action :nothing
+end.action(:create)
+
+template app_dir.join("shared", "config", "google_analytics.yml").to_s do
+  source "google_analytics.yml.erb"
+  owner recipe.original_user
+  group recipe.original_group
+  mode 0644
+  variables(:id => data_bag_item("analytics", "google")[node.name])
+  action :nothing
+end.action(:create)

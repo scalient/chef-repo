436c12b6151c92526fd4e6a2be8834abcffdbccc
Roy Liu <carsomyr@gmail.com>
2012-09-20 11:44:38 -0400

    scalient: Add the route53_record["register-a-wwwizer"] resource
    wwwizer.com is a "naked domain" redirect service.

--- vendor/cookbooks/scalient/recipes/aws.rb
+++ vendor/cookbooks/scalient/recipes/aws.rb
@@ -14,3 +14,19 @@ route53_record "register-cname" do
 
   action :nothing
 end.action(:create)
+
+if node.name.split(".", -1)[0] == "www"
+  # Enable the wwwizer.com "naked domain" redirect service.
+  route53_record "register-a-wwwizer" do
+    name node.name.split(".", -1)[1..-1].join(".")
+    type "A"
+    value "174.129.25.170"
+    ttl 604800
+
+    zone_id data_bag_item("dns", "aws")["route53_zone_id"]
+    aws_access_key_id data_bag_item("keys", "aws")["access_key"]
+    aws_secret_access_key data_bag_item("keys", "aws")["secret_key"]
+
+    action :nothing
+  end.action(:create)
+end

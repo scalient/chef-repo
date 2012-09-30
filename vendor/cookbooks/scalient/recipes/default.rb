f1a1a48d1be4d0daac3cb19198d4ccb3623803ae
Roy Liu <carsomyr@gmail.com>
2012-09-30 18:14:04 -0400

    scalient: Add resources for setting up chef-solo
    chef-solo will be used to augment the machine's boot sequence independent of the Chef server.

--- vendor/cookbooks/scalient/recipes/default.rb
+++ vendor/cookbooks/scalient/recipes/default.rb
@@ -2,8 +2,15 @@
 #
 # Copyright 2012 Scalient LLC
 
-require "pathname"
+class << self
+  include Scalient::Utils
+end
 
+require "pathname"
+require "shellwords"
+
+recipe = self
+prefix_dir = Pathname.new("/usr/local")
 gemfile = Pathname.new("client/Gemfile.d/Gemfile-scalient").expand_path(Dir.home("chef"))
 has_rabbitmq_server = Pathname.new("/usr/sbin/rabbitmqctl").executable?
 
@@ -66,3 +73,77 @@ ruby_block "set-ec2-instance-name" do
 
   action :nothing
 end.action(:create)
+
+[prefix_dir.join("etc", "chef-solo"),
+ prefix_dir.join("var", "chef-solo"),
+ prefix_dir.join("var", "chef-solo", "data_bags"),
+ prefix_dir.join("var", "chef-solo", "roles"),
+ prefix_dir.join("var", "log", "chef-solo")].each do |dir|
+  directory dir.to_s do
+    owner "chef"
+    group "chef"
+    mode 0755
+    action :nothing
+  end.action(:create)
+end
+
+template prefix_dir.join("etc", "chef-solo", "solo.rb").to_s do
+  source "solo.rb.erb"
+  owner "chef"
+  group "chef"
+  mode 0644
+  variables(:prefix => prefix_dir.to_s)
+  action :nothing
+end.action(:create)
+
+cookbook_file prefix_dir.join("etc", "chef-solo", "node.json").to_s do
+  source "node.json"
+  owner "chef"
+  group "chef"
+  mode 0644
+  action :nothing
+end.action(:create)
+
+cookbook_file prefix_dir.join("var", "chef-solo", "roles", "init.json").to_s do
+  source "init.json"
+  owner "chef"
+  group "chef"
+  mode 0644
+  action :nothing
+end.action(:create)
+
+[["dns", "aws"],
+ ["keys", "aws"]].each do |tuple|
+  directory prefix_dir.join("var", "chef-solo", "data_bags", tuple[0]).to_s do
+    owner "chef"
+    group "chef"
+    mode 0755
+    action :nothing
+  end.action(:create)
+
+  file prefix_dir.join("var", "chef-solo", "data_bags", tuple[0], "#{tuple[1]}.json").to_s do
+    content JSON.pretty_generate(data_bag_item(tuple[0], tuple[1]).raw_data)
+    owner "chef"
+    group "chef"
+    mode 0600
+    action :nothing
+  end.action(:create)
+end
+
+template "/etc/init/chef-solo.conf" do
+  source "chef-solo.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(:rbenv_version => Pathname.new("../..").expand_path(recipe.ruby_interpreter_path).basename.to_s,
+            :prefix => prefix_dir.to_s)
+  notifies :create, "link[/etc/init.d/chef-solo]", :immediately
+  action :nothing
+end.action(:create)
+
+link "/etc/init.d/chef-solo" do
+  to "/lib/init/upstart-job"
+  owner "root"
+  group "root"
+  action :nothing
+end

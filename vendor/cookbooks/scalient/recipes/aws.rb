f652bda4ea62bb791fd847b98b0f207e5790af0d
Roy Liu <carsomyr@gmail.com>
2013-07-29 19:04:37 -0400

    scalient: Change the aws recipe to accept alternate organization names

--- vendor/cookbooks/scalient/recipes/aws.rb
+++ vendor/cookbooks/scalient/recipes/aws.rb
@@ -2,33 +2,56 @@
 #
 # Copyright 2012 Scalient LLC
 
-org_name = node.name.split(".", -1)[1]
+recipe = self
+hostname = node.name.split(".", -1)[0]
+main_org_name = node.name.split(".", -1)[1]
+alternate_org_names = data_bag_item("dns", "aws")[main_org_name]["alternates"] || []
+access_key = data_bag_item("keys", "aws")[main_org_name]["access_key"]
+secret_key = data_bag_item("keys", "aws")[main_org_name]["secret_key"]
 
-route53_record "register-cname" do
-  name node.name
-  type "CNAME"
-  value node["ec2"]["public_hostname"]
-  ttl 300
+ruby_block "register-cnames" do
+  block do
+    require "fog"
 
-  zone_id data_bag_item("dns", "aws")[org_name]["route53_zone_id"]
-  aws_access_key_id data_bag_item("keys", "aws")[org_name]["access_key"]
-  aws_secret_access_key data_bag_item("keys", "aws")[org_name]["secret_key"]
+    zones = Fog::DNS.new({:provider => "aws",
+                          :aws_access_key_id => access_key,
+                          :aws_secret_access_key => secret_key}).zones
+
+    ([main_org_name] + alternate_org_names).each do |org_name|
+      zone_id = data_bag_item("dns", "aws")[org_name]["route53_zone_id"]
+      domain = zones.get(zone_id).domain.gsub(Regexp.new("\\.$"), "")
+      domain_name = hostname + "." + domain
+
+      recipe.route53_record "register-cname-#{domain_name}" do
+        name domain_name
+        type "CNAME"
+        value node["ec2"]["public_hostname"]
+        ttl 300
+
+        zone_id zone_id
+        aws_access_key_id access_key
+        aws_secret_access_key secret_key
+
+        action :nothing
+      end.action(:create)
+
+      if hostname == "www"
+        # Enable the wwwizer.com "naked domain" redirect service.
+        recipe.route53_record "register-a-wwwizer-#{domain_name}" do
+          name domain
+          type "A"
+          value "174.129.25.170"
+          ttl 604800
+
+          zone_id zone_id
+          aws_access_key_id access_key
+          aws_secret_access_key secret_key
+
+          action :nothing
+        end.action(:create)
+      end
+    end
+  end
 
   action :nothing
 end.action(:create)
-
-if node.name.split(".", -1)[0] == "www"
-  # Enable the wwwizer.com "naked domain" redirect service.
-  route53_record "register-a-wwwizer" do
-    name node.name.split(".", -1)[1..-1].join(".")
-    type "A"
-    value "174.129.25.170"
-    ttl 604800
-
-    zone_id data_bag_item("dns", "aws")[org_name]["route53_zone_id"]
-    aws_access_key_id data_bag_item("keys", "aws")[org_name]["access_key"]
-    aws_secret_access_key data_bag_item("keys", "aws")[org_name]["secret_key"]
-
-    action :nothing
-  end.action(:create)
-end
